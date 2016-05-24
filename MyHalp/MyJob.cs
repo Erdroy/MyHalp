597b122505eb6253120ba7dde1160db966b185e4
Erdroy <erdroy12@o2.pl>
2016-05-24 22:59:53 +0200

    Simple jobs, this still needs some work, but it works OK at this time.

---
+++ MyHalp/MyJob.cs
@@ -0,0 +1,19 @@
+﻿// MyHalp © 2016 Damian 'Erdroy' Korczowski, Mateusz 'Maturas' Zawistowski and contibutors.
+
+using System.Threading;
+
+namespace MyHalp
+{
+    public class MyJob
+    {
+        /// <summary>
+        /// Run new job.
+        /// </summary>
+        /// <param name="callback">Delegate or something. x => SomeMethod() or delegate{ some; code; }</param>
+        public static void Run(WaitCallback callback)
+        {
+            // TODO: Create own thread instead of using this.
+            ThreadPool.QueueUserWorkItem(callback);
+        }
+    }
+}
