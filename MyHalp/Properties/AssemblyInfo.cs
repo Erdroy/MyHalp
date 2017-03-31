c787c9478b031cf8aa16e0417ca3e9ce4c20eee6
Damian Korczowski <erdroy2@gmail.com>
2017-03-31 12:58:08 +0200

    updated assembly info

--- MyHalp/Properties/AssemblyInfo.cs
+++ MyHalp/Properties/AssemblyInfo.cs
@@ -4,14 +4,14 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("MyHalp")]
-[assembly: AssemblyDescription("MyHalp - Unity3D helper library")]
+[assembly: AssemblyDescription("MyHalp - Unity 5 helper library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MyHalp")]
-[assembly: AssemblyCopyright("MyHalp © 2016 Damian 'Erdroy' Korczowski, Mateusz 'Maturas' Zawistowski and contibutors.")]
+[assembly: AssemblyCopyright("MyHalp © 2016 Damian 'Erdroy' Korczowski and contibutors.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("dd420585-551f-4b13-949a-8e4dc220d37f")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.6")]
+[assembly: AssemblyFileVersion("1.6")]
