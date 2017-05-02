d2434c35f9b167eab8b1628c56c2f5281ef829af
Damian Korczowski <erdroy2@gmail.com>
2017-05-02 19:44:45 +0200

    added editor project for unity editor extensions

---
+++ MyHalp.Editor/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿// MyHalp © 2016-2017 Damian 'Erdroy' Korczowski
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyHalp.Editor")]
+[assembly: AssemblyDescription("MyHalp - Unity 5 helper library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyHalp")]
+[assembly: AssemblyCopyright("MyHalp © 2016 Damian 'Erdroy' Korczowski and contibutors.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d4d156bf-b73b-400c-80ec-6db8159fbda4")]
+[assembly: AssemblyVersion("1.6")]
+[assembly: AssemblyFileVersion("1.6")]
