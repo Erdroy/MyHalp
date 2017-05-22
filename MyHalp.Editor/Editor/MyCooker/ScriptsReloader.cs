d55808d743bc70e439c178c5095275a1a20cb09b
Damian Korczowski <erdroy2@gmail.com>
2017-05-22 18:44:09 +0200

    Found even better way to reload scripts - by a mistake but it works reliably.

---
+++ MyHalp.Editor/Editor/MyCooker/ScriptsReloader.cs
@@ -0,0 +1,18 @@
+﻿// MyHalp © 2016-2017 Damian 'Erdroy' Korczowski
+
+using UnityEditor;
+
+namespace MyHalp.Editor.MyCooker
+{
+    internal class ScriptsReloader
+    {
+        public static void ForceReload()
+        {
+            // I don't know how this works, but it works.
+            // I found this solution by a mistake.
+            // This file doesn't even exist, 
+            // so Unity probably thinks that it was removed.
+            AssetDatabase.ImportAsset("Assets/.hidden/RELOADSCRIPTS.cs");
+        }
+    }
+}
