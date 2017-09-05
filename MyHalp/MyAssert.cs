e4ab6911138fc5a1c22348aa3aea775f4d8695cc
Damian Korczowski <erdroy2@gmail.com>
2017-09-05 20:50:24 +0200

    Testing MyAssert class

---
+++ MyHalp/MyAssert.cs
@@ -0,0 +1,23 @@
+﻿// MyHalp © 2016-2017 Damian 'Erdroy' Korczowski
+
+namespace MyHalp
+{
+    /// <summary>
+    /// MyAssert class - implements loads of `.Assert` functions which can be used during runtime to check a wide variety of data types.
+    /// </summary>
+    public static class MyAssert
+    {
+        /// <summary>
+        /// Check `data` for null, if it's null it logs 'failMessage' as Error.
+        /// </summary>
+        public static void Assert(UnityEngine.Object data, string failMessage, MyLoggerLevel logLevel = MyLoggerLevel.Warning)
+        {
+            if (data == null)
+            {
+                MyLogger.Add(failMessage, logLevel);
+            }
+        }
+
+        // TODO: more implementations
+    }
+}
